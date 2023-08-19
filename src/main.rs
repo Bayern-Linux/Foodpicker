438769a4f04a120a570857d30f3ef52d56ee6716
Marion Hinkel <marion@fauxia.de>
2023-08-19 21:30:14 +0200

    Added basic webpage stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use crate::food_choice::{Affordability, Place};
+use actix_web::{App, HttpServer};
 
 mod food_choice;
 mod queries;
+mod webpage;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
     dotenvy::dotenv().ok();
+    HttpServer::new(|| App::new().service(webpage::index))
+        .bind("127.60.20.1:7373")?
+        .run()
+        .await?;
     let mut pool = sqlx::postgres::PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
     let food_choice = food_choice::FoodChoice {
         name: "Pizza".to_string(),
