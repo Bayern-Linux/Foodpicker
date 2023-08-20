a17cb4f36322ffb4101995d86118cd52ca362249
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2023-08-20 14:26:03 +0200

    Use shared AppState

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 use crate::food_choice::{Affordability, Place};
 use actix_web::{App, HttpServer};
+use sqlx::{Pool, Postgres};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 mod food_choice;
 mod maps;
 mod queries;
 mod webpage;
 
+struct AppState {
+    pool: Mutex<Pool<Postgres>>,
+}
+
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
     dotenvy::dotenv().ok();
-    HttpServer::new(|| App::new().service(webpage::index))
-        .bind("127.60.20.1:7373")?
-        .run()
-        .await?;
-    let mut pool = sqlx::postgres::PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
+    let pool = sqlx::postgres::PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
+    HttpServer::new(move || {
+        let app_state = AppState {
+            pool: Mutex::new(pool.clone()),
+        };
+        App::new().app_data(app_state).service(webpage::index)
+    })
+    .bind("127.60.20.1:7373")?
+    .run()
+    .await?;
     let food_choice = food_choice::FoodChoice {
         name: "Pizza".to_string(),
         price: Affordability::Low,
--- src/maps.rs
+++ src/maps.rs
@@ -1,9 +1,27 @@
 use google_maps::directions::Location;
 use google_maps::distance_matrix::TravelMode;
 use google_maps::prelude::Decimal;
+use google_maps::prelude::Place as MapsPlace;
+use tracing::info;
 
-pub async fn find_food_nearby(){
-    let maps_client = google_maps::GoogleMapsClient::new(std::env::var("MAPS_API_KEY").expect("MAPS_API_KEY not set").as_str());
-    let directions = maps_client.text_search("Pizza".to_string(), 5000).execute().await.unwrap();
-    directions.results.iter().filter(|x| x.rating.is_some_and(|x| x > Decimal::from_f32_retain(3.5).unwrap())).for_each(|x| println!("{:?}", x.name));
+pub async fn find_food_nearby() -> Result<Vec<MapsPlace>, google_maps::error::Error> {
+    let maps_client = google_maps::GoogleMapsClient::new(
+        std::env::var("MAPS_API_KEY")
+            .expect("MAPS_API_KEY not set")
+            .as_str(),
+    );
+    let restaurants = maps_client
+        .text_search("restaurants".to_string(), 5000)
+        .execute()
+        .await
+        .unwrap();
+    let filtered_restaurants = restaurants
+        .results
+        .into_iter()
+        .filter(|x| {
+            x.rating
+                .is_some_and(|x| x > Decimal::from_f32_retain(3.5).unwrap())
+        })
+        .collect();
+    Ok(filtered_restaurants)
 }
