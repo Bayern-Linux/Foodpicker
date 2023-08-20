a78ad8f9a147168947789da0182e7d5c3069f815
Marion Hinkel <marion@fauxia.de>
2023-08-20 13:47:40 +0200

    Added code to submit food choice on webpage

--- src/food_choice.rs
+++ src/food_choice.rs
@@ -1,18 +1,20 @@
-#[derive(sqlx::Type, Debug)]
+#[derive(sqlx::Type, Debug, serde::Serialize, serde::Deserialize)]
 #[sqlx(type_name = "affordability", rename_all = "lowercase")]
 pub enum Affordability {
     Low,
     Medium,
     High,
 }
-#[derive(sqlx::Type, Debug)]
+#[derive(sqlx::Type, Debug, serde::Serialize, serde::Deserialize)]
 #[sqlx(type_name = "place", rename_all = "lowercase")]
 pub enum Place {
     Home,
     Restaurant,
     Takeout,
 }
-#[derive(Debug, sqlx::FromRow, sqlx::Encode, sqlx::Decode)]
+#[derive(
+    Debug, sqlx::FromRow, sqlx::Encode, sqlx::Decode, serde::Serialize, serde::Deserialize,
+)]
 pub struct FoodChoice {
     pub(crate) name: String,
     // Cheap, Expensive, or Moderate
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,14 @@ async fn main() -> color_eyre::Result<()> {
     tracing_subscriber::fmt::init();
     color_eyre::install()?;
     dotenvy::dotenv().ok();
-    HttpServer::new(|| App::new().service(webpage::index))
-        .bind("127.60.20.1:7373")?
-        .run()
-        .await?;
+    HttpServer::new(|| {
+        App::new()
+            .service(webpage::index)
+            .service(webpage::send_food_choice)
+    })
+    .bind("127.60.20.1:7373")?
+    .run()
+    .await?;
     let mut pool = sqlx::postgres::PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
     let food_choice = food_choice::FoodChoice {
         name: "Pizza".to_string(),
